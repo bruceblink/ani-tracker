3e846286055fd4edb01362f404ceed1ad7bd4e2f
likanug <likanug.g@qq.com>
2025-08-04 14:12:10 +0800

    refactor: 更名blog的相关内容为favorite

--- src/views/favorite.rs
+++ src/views/favorite.rs
@@ -1,7 +1,7 @@
 use crate::Route;
 use dioxus::prelude::*;
 
-const BLOG_CSS: Asset = asset!("/assets/styling/blog.css");
+const FAVOR_CSS: Asset = asset!("/assets/styling/favorite.css");
 
 /// The Blog page component that will be rendered when the current route is `[Route::Blog]`
 ///
@@ -10,7 +10,7 @@ const BLOG_CSS: Asset = asset!("/assets/styling/blog.css");
 #[component]
 pub fn Favorite(id: i32) -> Element {
     rsx! {
-        document::Link { rel: "stylesheet", href: BLOG_CSS }
+        document::Link { rel: "stylesheet", href: FAVOR_CSS }
 
         div {
             id: "favorite",
