64dd65bc849daf92bcdd54f40b16398f7e79fafd
likanug <likanug.g@qq.com>
2025-08-04 15:15:21 +0800

    refactor: 使用AniItem component重构AniList

--- src/components/ani_info.rs
+++ src/components/ani_info.rs
@@ -1,22 +1,49 @@
 use dioxus::prelude::*;
 
-const HEADER_SVG: Asset = asset!("/assets/header.svg");
+#[derive(Clone)]
+struct AniData {
+    title: String,
+    url:   String,
+}
+
+#[derive(Props, PartialEq, Clone)]
+struct AniProps {
+    #[props(into)]
+    title: String,
+    #[props(into)]
+    url:   String,
+}
+
+#[component]
+fn AniItem(props: AniProps) -> Element {
+    let AniProps { title, url } = props;
+    rsx! {
+        a { href: "{url}", "{title}" }
+    }
+}
 
 #[component]
 pub fn AniList() -> Element {
+    let items = vec![
+        AniData { title: "📚 Learn Dioxus".into(), url: "https://dioxuslabs.com/learn/0.6/".into() },
+        AniData { title: "🚀 Awesome Dioxus".into(), url: "https://dioxuslabs.com/awesome".into() },
+        AniData { title: "📡 Community Libraries".into(), url: "https://github.com/dioxus-community/".into() },
+        AniData { title: "⚙️ Dioxus Development Kit".into(), url: "https://github.com/DioxusLabs/sdk".into() },
+        AniData { title: "💫 VSCode Extension".into(), url: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus".into() },
+        AniData { title: "👋 Community Discord".into(), url: "https://discord.gg/XgGxMSkvUM".into() },
+    ];
+
     rsx! {
-        // We can create elements inside the rsx macro with the element name followed by a block of attributes and children.
-        div {
-            // Attributes should be defined in the element before any children
-            id: "ani-list",
+        div { id: "ani-list",
             div { id: "links",
-                // The RSX macro also supports text nodes surrounded by quotes
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+                // 使用 for 循环渲染列表项
+                for data in items.iter() {
+                    AniItem {
+                        key:   "{data.title}",
+                        title: "{data.title}",
+                        url:   "{data.url}",
+                    }
+                }
             }
         }
     }
