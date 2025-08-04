985a7c2d1c945df07e30bfb988330e49691dd81f
likanug <likanug.g@qq.com>
2025-08-04 16:33:40 +0800

    feat: 重新实现Search功能，使之可以返回查询结果到其他component

--- src/components/search.rs
+++ src/components/search.rs
@@ -1,16 +1,23 @@
 use dioxus::prelude::*;
-
+use crate::components::ani_info::AniData;
 const ECHO_CSS: Asset = asset!("/assets/styling/search.css");
+
+
+#[derive(Props, PartialEq)]
+#[derive(Clone)]
+pub struct SearchProps {
+    pub on_search: EventHandler<String>,
+}
 
 /// Search component that demonstrates fullstack server functions.
 #[component]
-pub fn Search() -> Element {
+pub fn Search(props: SearchProps) -> Element {
     // use_signal is a hook. Hooks in dioxus must be run in a consistent order every time the component is rendered.
     // That means they can't be run inside other hooks, async blocks, if statements, or loops.
     //
     // use_signal is a hook that creates a state for the component. It takes a closure that returns the initial value of the state.
     // The state is automatically tracked and will rerun any other hooks or components that read it whenever it changes.
-    let mut response = use_signal(|| String::new());
+    let SearchProps { on_search } = props;
 
     rsx! {
         document::Link { rel: "stylesheet", href: ECHO_CSS }
@@ -22,25 +29,7 @@ pub fn Search() -> Element {
                 placeholder: "Type here to search...",
                 // `oninput` is an event handler that will run when the input changes. It can return either nothing or a future
                 // that will be run when the event runs.
-                oninput:  move |event| async move {
-                    // When we call the echo_server function from the client, it will fire a request to the server and return
-                    // the response. It handles serialization and deserialization of the request and response for us.
-                    let data = search_server(event.value()).await.unwrap();
-
-                    // After we have the data from the server, we can set the state of the signal to the new value.
-                    // Since we read the `response` signal later in this component, the component will rerun.
-                    response.set(data);
-                },
-            }
-
-            // Signals can be called like a function to clone the current value of the signal
-            if !response().is_empty() {
-                p {
-                    "Server echoed: "
-                    // Since we read the signal inside this component, the component "subscribes" to the signal. Whenever
-                    // the signal changes, the component will rerun.
-                    i { "{response}" }
-                }
+                oninput: move |e| on_search.call(e.value().to_string()),
             }
         }
     }
@@ -53,9 +42,21 @@ pub fn Search() -> Element {
 // When the server function is called from the client, it will just serialize the arguments, call the API, and deserialize the
 // response.
 #[server]
-async fn search_server(input: String) -> Result<String, ServerFnError> {
+pub async fn search_server(input: String) -> Result<Vec<AniData>, ServerFnError> {
     // The body of server function like this comment are only included on the server. If you have any server-only logic like
     // database queries, you can put it here. Any imports for the server function should either be imported inside the function
     // or imported under a `#[cfg(feature = "server")]` block.
-    Ok(input)
+    let items = vec![
+        AniData { title: "📚 Learn Dioxus".into(), url: "https://dioxuslabs.com/learn/0.6/".into() },
+        AniData { title: "🚀 Awesome Dioxus".into(), url: "https://dioxuslabs.com/awesome".into() },
+        AniData { title: "📡 Community Libraries".into(), url: "https://github.com/dioxus-community/".into() },
+        AniData { title: "⚙️ Dioxus Development Kit".into(), url: "https://github.com/DioxusLabs/sdk".into() },
+        AniData { title: "💫 VSCode Extension".into(), url: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus".into() },
+        AniData { title: "👋 Community Discord".into(), url: "https://discord.gg/XgGxMSkvUM".into() },
+    ];
+    let filtered_ani = items.iter()
+        .filter(|item| item.title.contains(&input))
+        .cloned()
+        .collect::<Vec<AniData>>();
+    Ok(filtered_ani)
 }
