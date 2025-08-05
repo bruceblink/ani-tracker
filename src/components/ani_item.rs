732e58bef4433ceca39aa5aeb528e893cd666091
likanug <likanug.g@qq.com>
2025-08-05 14:05:09 +0800

    feat: 更新AniItem的页面样式和组件属性

--- src/components/ani_item.rs
+++ src/components/ani_item.rs
@@ -5,13 +5,50 @@ pub struct AniProps {
     #[props(into)]
     title: String,
     #[props(into)]
-    url:   String,
+    detail_url: String,
+    #[props(into)]
+    update_count: String,
+    #[props(into)]
+    update_info: String,
+    #[props(into)]
+    image_url: String,
+    #[props(into)]
+    update_time: String,
+    #[props(into)]
+    platform: String,
 }
 
 #[component]
 pub fn AniItem(props: AniProps) -> Element {
-    let AniProps { title, url } = props;
+    let AniProps {
+        title,
+        detail_url,
+        update_count,
+        update_info,
+        image_url,
+        update_time,
+        platform,
+    } = props;
+    
     rsx! {
-        a { href: "{url}", "{title}" }
+        div {
+            class: "ani-item",
+            id: "ani-item-{title}",
+            style: "border: 1px solid #ccc; 
+                    padding: 10px; 
+                    margin-bottom: 10px; 
+                    display: flex;
+                    align-items: center;",
+            
+            //a { href: "{detail_url}", "{title}" }
+            img {
+                src: "{image_url}",
+                alt: "{title}",
+                style: "width: 92.7px; height: 150px; object-fit: cover;",
+                class: "ani-item-image"
+            }
+            h3 { "{title}" }
+        }
+
     }
 }
