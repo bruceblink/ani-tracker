b788038525686106f82000f0938f7e1af933d531
likanug <likanug.g@qq.com>
2025-08-05 17:56:52 +0800

    refactor: 更新ani-list的样式

--- src/components/ani_list.rs
+++ src/components/ani_list.rs
@@ -12,24 +12,28 @@ pub fn AniList(items: Vec<AniData>) -> Element {
                        flexWrap: wrap;
                        gap: min(24px, 2vw);
                        justifyContent: center;
-                       padding: 120px 0 24px;
+                       padding: 80px 0 24px;
                        margin: 0 auto;
                        width: 100%;
                        maxWidth: 90%;",
-            div { 
+            
                 // 使用 for 循环渲染列表项
                 for data in items.iter() {
-                    AniItem {
-                        key:   {data.title},
-                        title: {data.title.clone()},
-                        update_count: {data.update_count.clone()},
-                        detail_url:   {data.detail_url.clone()},
-                        update_info:  {data.update_info.clone()},
-                        image_url:    {data.image_url.clone()},
-                        update_time:  {data.update_time.clone()},
-                        platform:     {data.platform.clone()},
+                    div { 
+                        style: "width: calc(clamp(360px, calc(90vw/4 - 24px), 360px));
+                                height: calc(calc(clamp(360px, calc(90vw/4 - 24px), 360px) * 0.618));
+                                flexShrink: 0;",
+                        AniItem {
+                            key:   {data.title},
+                            title: {data.title.clone()},
+                            update_count: {data.update_count.clone()},
+                            detail_url:   {data.detail_url.clone()},
+                            update_info:  {data.update_info.clone()},
+                            image_url:    {data.image_url.clone()},
+                            update_time:  {data.update_time.clone()},
+                            platform:     {data.platform.clone()},
+                        }
                     }
-                }
             }
         }
     }
