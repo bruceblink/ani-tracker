455af05ba95fc6a119a5b0791a8a0e2b9d32873a
likanug <likanug.g@qq.com>
2025-08-11 10:45:39 +0800

    feat: 添加app.css，更新notfound页面的样式布局，统一样式

--- src/views/notfound.rs
+++ src/views/notfound.rs
@@ -1,22 +1,22 @@
 use dioxus::prelude::*;
 
-const FAVOR_CSS: Asset = asset!("/assets/styling/favorite.css");
+const APP_CSS: Asset = asset!("/assets/styling/app.css");
 
-/// The Blog page component that will be rendered when the current route is `[Route::Blog]`
-///
-/// The component takes a `id` prop of type `i32` from the route enum. Whenever the id changes, the component function will be
-/// re-run and the rendered HTML will be updated.
 #[component]
 pub fn NotFound(route: Vec<String>) -> Element {
     rsx! {
-        document::Link { rel: "stylesheet", href: FAVOR_CSS }
-        h1 { "404 - 页面未找到" }
-        p { "您请求的页面不存在：" }
-        ul {
-            for segment in route {
-                li { "{segment}" }
+        document::Link { rel: "stylesheet", href: APP_CSS }
+        div {
+            class: "not-found",
+            h1 { "404 - 页面未找到" }
+            p { "您请求的页面不存在：" }
+            ul {
+                for segment in route {
+                    "{segment}"
+                }
             }
         }
+
     }
 
 }
