83340f599cd423a484537febafc24f01273a33f6
likanug <likanug.g@qq.com>
2025-08-26 22:00:23 +0800

    refactor: 新增service模块用于写后端的业务代码

---
+++ src/backend/service/ani_info.rs
@@ -0,0 +1,28 @@
+use dioxus::prelude::{server, ServerFnError};
+use crate::backend::po::Ani;
+
+#[server(endpoint = "list_ani_info")]
+pub async fn list_ani_info() -> Result<Vec<(usize, String)>, ServerFnError> {
+
+    let _sql = sqlx::query_as::<_, Ani>(r#"
+                SELECT ai.id,
+                       ai.title,
+                       ai.update_count,
+                       ai.update_info,
+                       ai.image_url,
+                       ai.detail_url,
+                       ai.update_time,
+                       ai.platform
+                FROM ani_info ai
+                WHERE ai.update_time >= ?
+           ;"#,
+    );
+    
+    
+    let ani_info_list = vec![
+        (1, "Naruto".to_string()),
+        (2, "One Piece".to_string()),
+        (3, "Attack on Titan".to_string()),
+    ];
+    Ok(ani_info_list)
+}
